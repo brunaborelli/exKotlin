69bf7ed3b8f7431a77b34561cbc8e8604ec31916
Borelli <bruhborelli6@gmail.com>
2022-02-21 13:20:46 -0300

    ex1 kotlin

---
+++ primeiroProjetoGen/src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    println("Bruna - E aí, gente! Quando vamos na hamburgueria da Gi?")
+    println("Leticia - Vcs tem disponibilidade pra ir hoje?")
+    println("Gabriele - Está chovendo muito por aqui, o metrô parou há quase 1 hora")
+    println("Celina - Puts, melhor nem arriscar então")
+    println("Gabriele - Sim, infelizmente")
+    println("Bruna - Eu tenho sim, Le")
+    println("Celina - Eu também")
+    println("Leticia - Bora hoje então?")
+    println("Bruna - Bora bora, vou ligar pra Nathália")
+    println("Celina - A gente se encontra lá?")
+    println("Bruna - Que tal nos encontrarmos na Luz?")
+    println("Leticia - Por mim, fechou! 19h?")
+    println("Celina - Belezinha")
+    println("Bruna - Perfeito, Nathalia também confirmou")
+    println("Leticia - Até maiiis!")
+}
